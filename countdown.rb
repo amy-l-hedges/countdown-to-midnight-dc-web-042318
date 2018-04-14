f9a3ec805c190bbf40b577f4ec44d369e232e042
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-14 13:40:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,20 +5,5 @@ def countdown(num_of_seconds)
   phrase = "#{number_of_seconds} seconds!"
   while counter > 0
   puts phrase
-  counter = counter =- 1
-end
-
-def while_iterator(number_of_times)
-  counter = 0
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  while counter < number_of_times
-  puts phrase
-  number_of_times += 1
-end
-end
-
-x = 8
-while x > 0
-  x = x-1
- puts x
+  counter = counter -= 1
 end

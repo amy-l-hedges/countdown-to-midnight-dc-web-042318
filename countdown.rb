4c8be8d76a94477e019d8f75baf7aa60fe917efb
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-14 14:11:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(num_of_seconds)
 end
 return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num_of_seconds)
+  sleep 1
+  while 0 < num_of_seconds
+  phrase = "#{num_of_seconds} SECOND(S)!"
+  puts phrase
+  num_of_seconds -= 1
+end
+return "HAPPY NEW YEAR!"
+end

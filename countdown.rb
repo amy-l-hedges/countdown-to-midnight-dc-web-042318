8fb3593f815576647a3462983193fd2954a10379
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-14 14:05:05 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown(num_of_seconds)
-  phrase = "#{num_of_seconds} SECOND(S)!"
   while 0 < num_of_seconds
+  phrase = "#{num_of_seconds} SECOND(S)!"
   sleep 1
   puts phrase
   num_of_seconds -= 1
